b9e07e8b7d016a5c63bee05a9072eb61ce76bcb5
SirPeace12 <94797191+SirPeace12@users.noreply.github.com>
2023-06-23 01:32:29 -0500

    se ajusta el archivo de views de authenticacion

---
+++ app/authentication/api_v1_0/views.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo,ObjectId
+from config import db
+
+
+def login():
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'email': doc['email'],
+            'password': doc['password']
+        })
+    return jsonify(users)
