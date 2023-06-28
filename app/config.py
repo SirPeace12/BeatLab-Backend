1f0f449fbf7bb3e15c3fda08e79fea52abeed513
Jaime Alberto López Restrepo <lopezjaime433@gmail.com>
2023-06-28 12:23:29 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo,ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, methods=['GET', 'POST', 'PUT', 'DELETE'], supports_credentials=True)
+CORS(app, methods=['GET', 'POST', 'PUT', 'DELETE'], supports_credentials=True, origins="*", allow_headers=["Content-Type"])
 app.config['MONGO_URI']='mongodb://localhost/BeatLab'
 mongo = PyMongo(app)
 db = mongo.db.users
