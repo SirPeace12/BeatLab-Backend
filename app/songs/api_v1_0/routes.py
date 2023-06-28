d043a2fade71f7acdacb0267276a7141650687c9
SirPeace12 <94797191+SirPeace12@users.noreply.github.com>
2023-06-28 16:54:21 -0500

    se ajusta el arcivo de canciones

---
+++ app/songs/api_v1_0/routes.py
@@ -0,0 +1,8 @@
+# from flask import Blueprint
+# import songs.api_v1_0.views  as songs
+
+# songs_routes = Blueprint("songs", __name__)
+
+# @songs_routes.route("/songs", methods=["POST"])
+# def upload():
+#     return songs.upload()
