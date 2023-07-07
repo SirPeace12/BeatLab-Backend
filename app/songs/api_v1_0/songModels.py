3e7807cf5a1c3b5ed3950b30f66c5632310cfb2e
SirPeace12 <94797191+SirPeace12@users.noreply.github.com>
2023-07-06 23:49:18 -0500

    se corrige el modelo con datos default

--- app/songs/api_v1_0/songModels.py
+++ app/songs/api_v1_0/songModels.py
@@ -2,10 +2,10 @@
 from config import mongoengine
 
 class Song(mongoengine.Document):
-    title = mongoengine.StringField()
-    artist = mongoengine.StringField()
-    gender = mongoengine.StringField()
+    title = mongoengine.StringField(default="Desconocido")
+    artist = mongoengine.StringField(default="Desconocido")
+    gender = mongoengine.StringField(default="Desconocido")
     favorite = mongoengine.BooleanField(default=False)
     songURL = mongoengine.StringField()
-    imageSongURL = mongoengine.StringField()
+    imageSongURL = mongoengine.StringField(default="https://i.postimg.cc/4NBdd9NF/Beat-Lab-Logo.png")
     user = mongoengine.StringField()
